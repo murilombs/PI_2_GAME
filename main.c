595e2a30eea15b6c030dbf498e76023611627039
Dominique Xavier <dominiquexavier@outlook.com.br>
2023-11-28 21:58:18 -0300

    main atualizado com mapa

--- main.c
+++ main.c
@@ -1,192 +1,246 @@
-#include <allegro5/allegro5.h>
-#include <allegro5/allegro_font.h>
 #include <stdio.h>
-#include <time.h>
-#include <locale.h>
+#include <stdlib.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
 
-#include "abobora.h"
-#include "manipuladoresGene.h"
+#define TILE_SIZE 92
+
+char textoDebug[2000];
+
+void must_init(bool inicializacao_bem_sucedida, const char* description) {
+
+	if (inicializacao_bem_sucedida) return;
+	printf("Erro ao inicializar %s.", description);
+	exit(1);
+}
+
+void exibir_mensagem(ALLEGRO_FONT* font, const char* mensagem) {
+	al_draw_textf(font, al_map_rgb(255, 255, 255), 640, 50, ALLEGRO_ALIGN_CENTER, "%s\n", mensagem);
+	al_flip_display();
+	al_rest(1.0);
+
+}
+
+void tooltip(ALLEGRO_FONT* font, const char* texto, int mouse_x, int mouse_y) {
+	int largura = 220;
+	int altura = 100;
+
+	al_draw_filled_rectangle(mouse_x, mouse_y, mouse_x + largura, mouse_y + altura, al_map_rgb(3, 181, 24));
+	al_draw_textf(font, al_map_rgb(0, 0, 0), mouse_x + 15, mouse_y + 10, 20, "%s", texto);
+}
+
+void checaClickAbobora(int matriz[3][14], int inicio_x, int fim_y, int mouse_x, int mouse_y) {
+	for (int linha = 0; linha < 3; linha++) {
+		for (int coluna = 0; coluna < 14; coluna++) {
+
+			int x = inicio_x + coluna * TILE_SIZE;
+			int y = fim_y + linha * TILE_SIZE;
+
+			if (mouse_x > x &&
+				mouse_y > y &&
+				mouse_x < x + TILE_SIZE &&
+				mouse_y < y + TILE_SIZE) {
+				printf("Clicando em cima do quadrado de x: %d e y: %d\n", linha, coluna);
+			}
+		}
+	}
+}
+
+void desenharMatriz(int matriz[3][14], int inicio_x, int fim_y, int mouse_x, int mouse_y, ALLEGRO_BITMAP* abob, ALLEGRO_EVENT_QUEUE* queue) {
+
+	for (int linha = 0; linha < 3; linha++) {
+		for (int coluna = 0; coluna < 14; coluna++) {
+
+			int x = inicio_x + coluna * TILE_SIZE;
+			int y = fim_y + linha * TILE_SIZE;
+
+			if (matriz[linha][coluna] == 1) {
+				al_draw_filled_rectangle(x, y, x + TILE_SIZE, y + TILE_SIZE, al_map_rgb(163, 210, 119));
+				//al_draw_bitmap(abob, x, y, 0);
+			}
+			else if (matriz[linha][coluna] == 2) {
+				al_draw_filled_rectangle(x, y, x + TILE_SIZE, y + TILE_SIZE, al_map_rgb(125, 86, 64));
+				if ((mouse_x >= x && mouse_x <= x + TILE_SIZE) && (mouse_y >= y && mouse_y <= y + TILE_SIZE)) {
+					al_draw_filled_rectangle(x, y, x + TILE_SIZE, y + TILE_SIZE, al_map_rgb(225, 131, 60));
+				}
+			}
+			else if (matriz[linha][coluna] == 3) {
+				al_draw_filled_rectangle(x, y, x + TILE_SIZE, y + TILE_SIZE, al_map_rgb(112, 83, 57));
+				if ((mouse_x >= x && mouse_x <= x + TILE_SIZE) && (mouse_y >= y && mouse_y <= y + TILE_SIZE)) {
+					al_draw_filled_rectangle(x, y, x + TILE_SIZE, y + TILE_SIZE, al_map_rgb(225, 131, 60));
+				}
+			}
+		}
+	}
+
+}
+
+void interacaoMatriz(int matriz[3][14], int inicio_x, int fim_y, int mouse_x, int mouse_y, ALLEGRO_EVENT_QUEUE* queue) {
+
+}
 
 
 int main() {
 
-	setlocale(LC_ALL, "Portuguese");
+	must_init(al_init(), "Allegro"); // Inicialização do Allegro
 
-	//Inicializa o programa
-	al_init();
+	must_init(al_init_font_addon(), "addons de fonte"); // Inicializa os addons de fonte
 
-	if (!al_init()) {
-		printf("Erro ao inicializar o Allegro\n");
-		return 1;
-	}
+	must_init(al_init_ttf_addon(), "addons do Allegro"); // Inicializa os addons do Allegro
 
-	//Habilita receber dado do teclado
-	al_install_keyboard();
+	// Inicializa, carrega a fonte True Type + caminho relativo
+	ALLEGRO_FONT* font = al_load_ttf_font("goethe\\GoetheBold.ttf", 28, 0);
+	must_init(font, "fonte");
 
-	if (!al_install_keyboard()) {
-		printf("Erro ao inicializar o teclado\n");
-		return 1;
-	}
+	// Inicialização, criação do display e dimensões
+	ALLEGRO_DISPLAY* display = al_create_display(1280, 720);
+	must_init(display, "display");
 
-	//Cria um timer
-	//O timer atualiza a cada 1s
-	ALLEGRO_TIMER* timer = al_create_timer(1.0);
-
-	if (!timer) {
-		printf("Erro ao inicializar o timer\n");
-		return 1;
-	}
-
-	//Cria uma filha de eventos
+	// Inicialização e criação da fila de eventos
 	ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
+	must_init(queue, "fila de eventos");
 
-	if (!queue) {
-		printf("Erro ao inicializar a fila de eventos\n");
-		return 1;
-	}
+	// Inicializalção dos addons de imagem
+	must_init(al_init_image_addon(), "addons de imagem");
 
-	//Cria uma janela
-	ALLEGRO_DISPLAY* disp = al_create_display(900, 700);
+	// Inicializa e carrega a imagem
+	ALLEGRO_BITMAP* abob = al_load_bitmap("abob.png");
+	must_init(abob, "abob");
 
-	if (!disp) {
-		printf("Erro ao inicializar a janela\n");
-		return 1;
-	}
+	must_init(al_init_primitives_addon(), "primitives addon"); // Inicialização dos primitives
 
-	//Inicializa a fonte padrao
-	ALLEGRO_FONT* font = al_create_builtin_font();
+	// Inicialização e criação do timer
+	ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
+	must_init(timer, "timer");
 
-	if (!font) {
-		printf("Erro ao inicializar a fonte\n");
-		return 1;
-	}
+	must_init(al_install_keyboard(), "teclado"); // Inicializa e recebe comandos do teclado
+	must_init(al_install_mouse(), "mouse"); // Inicializa e recebe comandos do mouse
 
-	//al_register_event é usado para registrar eventos de uma fonte especifica (display, teclado etc) 
-	al_register_event_source(queue, al_get_keyboard_event_source());
-	al_register_event_source(queue, al_get_display_event_source(disp));
-	al_register_event_source(queue, al_get_timer_event_source(timer));
+	al_register_event_source(queue, al_get_keyboard_event_source()); // Recebe eventos do teclado
+	al_register_event_source(queue, al_get_mouse_event_source()); // Recebe eventos do mouse
+	al_register_event_source(queue, al_get_display_event_source(display)); // Recebe eventos do display
+	al_register_event_source(queue, al_get_timer_event_source(timer)); // Recebe eventos do timer
 
-	/*
-	> A variavel done é usada para controlar quando o programa deve ser encerrado
-	Ao ser definida como "false", o programa entende que não deve ser encerrado
-	> A variável "redraw" é usada para controlar quando a tela deve ser redesenhada
-	Ao ser definida como "true", o programa entende que no início ela deve ser reinicializada
-	> A variável "event" em ALLEGRO_EVENT é usada para armazenar eventos. É nele que os eventos
-	pendentes ficam armazenados e eventualmente processados
-	*/
 
-	bool done = false;
-	bool redraw = true;
 	ALLEGRO_EVENT event;
 
-	// > Variável do contador regressivo
-	int contador = 300; //definido em 5 min
-	// > variavel para pular linhas em exibição de lista de genes
-	int espacoY = 0;
+	bool done = false; // done será a variável que fechará o programa.
+	bool redraw = true; // Variável para redesenhar a tela a cada x segundos
 
-	//Inicializa o contador criado anteriormente
-	al_start_timer(timer);
+	int mouse_x = 0; // Coordenada de x (eixo vertical)
+	int mouse_y = 0; // Coordenada de y (eixo horizontal)
 
-	time_t segundos;
-	//time(&segundos);
-	//struct tm* data_hora_atual = localtime(&segundos);
+	bool mostrar_tooltip = false; // O tooltip será visível quando a variável for true
 
-	struct Abobora aboboras[40];
+	bool arrastando = false;
 
-	int *aboborasCriadas = 4;
-
-	for (int i = 0; i < aboborasCriadas; i++) {
-		gera_abobora_code(&aboboras[i], i);
-		for (int p1 = 0; p1 < 4; p1++) {
-			for (int p2 = 0; p2 < 2; p2++) {
-				guarda_gene(&aboboras[i], p1, p2);
-			}
-		}
-		guarda_caracteristicas(&aboboras[i]);
-		tempoDeCiclo(&aboboras[i], time(&segundos), 10);
+	// Matriz da plantação
+	int matriz[3][14] = {
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 1},
+	{1, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 1}
 	};
 
-	int code1, code2, filhas;
+	while (1) { // Loop principal
 
-	filhas = 4; // quantas filhas cada cruzamento vai criar
 
-	while (1) {
+
 		al_wait_for_event(queue, &event);
 
-		//Caso ocorra um evento x, tal coisa deverá ocorrer
+		al_clear_to_color(al_map_rgb(255, 255, 255));
+		desenharMatriz(matriz, 0, 444, mouse_x, mouse_y, abob, queue);
+
+		// Caso ocorra um evento x, tal coisa deve acontecer
 		switch (event.type) {
+
+		case ALLEGRO_EVENT_KEY_DOWN: // Caso uma tecla seja pressionada...
+			if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE) { // Se a tecla pressionada for ESC o programa fecha
+
+				done = true;
+			}
+			break;
+
+		case ALLEGRO_EVENT_DISPLAY_CLOSE: // Se o display for fechado, o programa acaba
+			done = true;
+
+			break;
+
+		case ALLEGRO_EVENT_MOUSE_AXES: // Guarda na fila de eventos do mouse as coordenadas do cursor
+			mouse_x = event.mouse.x;
+			mouse_y = event.mouse.y;
+
+			if ((mouse_x >= 960 && mouse_x <= 1920) && (mouse_y >= 520 && mouse_y <= 720)) {
+				mostrar_tooltip = true;
+				al_start_timer(timer);
+			}
+			else {
+				mostrar_tooltip = false;
+				al_stop_timer(timer);
+			}
+
+			if (arrastando) {
+				mouse_x += event.mouse.dx;
+				mouse_y += event.mouse.dy;
+
+			}
+
+			break;
 
 		case ALLEGRO_EVENT_TIMER:
 
-			//Passo do contador
-			contador--;
+			if ((al_get_timer_started(timer)) && (mostrar_tooltip)) {
+				tooltip(font, "Essa \u00e9 uma tooltip!", mouse_x, mouse_y);
 
-			if (contador <= 0) {
-				//Desativa o temporizador e encerra o programa quando o contador chega a 0s
-				al_stop_timer(timer);
-				done = true;
-			}
-			redraw = true;
-			break;
-
-		case ALLEGRO_EVENT_KEY_DOWN:
-
-			//Encerra o programa quando ESC é pressionado
-			if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE) {
-				done = true;
 			}
 
 			break;
 
-		case ALLEGRO_EVENT_DISPLAY_CLOSE:
+		case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN:
 
-			done = true;
-			break;
-		}
+			mouse_x = event.mouse.x;
+			mouse_y = event.mouse.y;
+			printf("x = %d\ny = %d\n\n", mouse_x, mouse_y);
 
+			if (event.mouse.button & 1) {
+				if ((event.mouse.x >= 480 && event.mouse.x <= 800) && (event.mouse.y >= 300 && event.mouse.y <= 600)) {
+					arrastando = true;
 
-		if (done)
-			break;
-
-		if (redraw && al_is_event_queue_empty(queue)) {
-			//Define a cor de fundo
-			al_clear_to_color(al_map_rgb(255, 219, 245));
-
-			// al_draw_textf -> fonte , cor da fonte, posição em X, posição em Y, alinhamento, tipo
-			for (int i = 0; i < aboborasCriadas; i++) {
-				al_draw_textf(font, al_map_rgb(0, 0, 0), 100, 10,
-					ALLEGRO_ALIGN_CENTER, "%d", aboboras[i].semente.aboboraCode);
+				}
 			}
 
-			//Atualiza a tela
-			al_flip_display();
+			checaClickAbobora(matriz, 0, 444, mouse_x, mouse_y);
 
-			redraw = false;
-		}
 
-		displayTodasAboboras(aboborasCriadas, &aboboras);
-		code1 = -1;
+			break;
 
-		code2 = -1;
+		case ALLEGRO_EVENT_MOUSE_BUTTON_UP:
+			arrastando = false;
 
-		while (code1 <= 1 || code1 >= aboborasCriadas && code2 <= 1 || code2 >= aboborasCriadas) {
-
-			printf("Entre com o código da Primeira abobora: ");
-			scanf("%d", &code1);
-
-			printf("Entre com o código da Segunda abobora: ");
-			scanf("%d", &code2);
-
-			cruzamento(code1, code2, filhas, &aboborasCriadas, aboboras, time(&segundos));
-
-			mudancaDeCiclo(&aboboras, aboborasCriadas, time(&segundos));
-			displayTodasAboboras(aboborasCriadas, &aboboras);
 			break;
 		}
+
+		if (done) { // Caso done seja true, o programa fecha.
+
+			break;
+
+		}
+
+		al_draw_text(font, al_map_rgb(0, 0, 0), 10, 10, 0, textoDebug);
+
+		al_flip_display();
+
+		al_clear_to_color(al_map_rgb(255, 255, 255));
+
+		al_set_target_bitmap(al_get_backbuffer(display));
+
 	}
 
 	al_destroy_font(font);
-	al_destroy_display(disp);
+	al_destroy_display(display);
+	al_destroy_event_queue(queue);
 	al_destroy_timer(timer);
 
 	return 0;
